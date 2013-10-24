9eff22340173b7e263f1197dea1a7e4941268280
MStuv <markstuver@MacBook-Pro.local>
2013-10-23 22:59:49 -0400

    Initial Commit

---
+++ Passing Data Challenge Solution/MASViewController.h
@@ -0,0 +1,13 @@
+//
+//  MASViewController.h
+//  Passing Data Challenge Solution
+//
+//  Created by Mark Stuver on 10/23/13.
+//  Copyright (c) 2013 Halo International Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MASViewController : UIViewController
+
+@end
---
+++ Passing Data Challenge Solution/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Passing Data Challenge Solution
+//
+//  Created by Mark Stuver on 10/23/13.
+//  Copyright (c) 2013 Halo International Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MASAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MASAppDelegate class]));
+    }
+}
